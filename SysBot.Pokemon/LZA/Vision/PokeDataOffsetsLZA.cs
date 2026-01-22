1f7c3995ff19d8f55d9ee79190382c271f702d04
Lusamine <30205550+Lusamine@users.noreply.github.com>
2026-01-21 21:02:14 -0600

    Update pointers for LZA 2.0.1

--- SysBot.Pokemon/LZA/Vision/PokeDataOffsetsLZA.cs
+++ SysBot.Pokemon/LZA/Vision/PokeDataOffsetsLZA.cs
@@ -7,23 +7,23 @@ namespace SysBot.Pokemon;
 /// </summary>
 public class PokeDataOffsetsLZA
 {
-    public const string LZAGameVersion = "2.0.0";
+    public const string LZAGameVersion = "2.0.1";
     public const string LegendsZAID = "0100F43008C44000";
 
-    public IReadOnlyList<long> BoxStartPokemonPointer           { get; } = [0x6105710, 0xB0, 0x978, 0x0];
-    public IReadOnlyList<long> TextSpeedPointer                 { get; } = [0x6105710, 0xD8, 0x40];
-    public IReadOnlyList<long> MyStatusPointer                  { get; } = [0x6105710, 0xA0, 0x40];
-    public IReadOnlyList<long> PartyPointer                     { get; } = [0x6105710, 0x18, 0x1B0, 0xF0, 0x50, 0x30, 0x0];
-    public IReadOnlyList<long> CurrentBoxPointer                { get; } = [0x6105710, 0xA8, 0x596];
+    public IReadOnlyList<long> BoxStartPokemonPointer           { get; } = [0x610A710, 0xB0, 0x978, 0x0];
+    public IReadOnlyList<long> TextSpeedPointer                 { get; } = [0x610A710, 0xD8, 0x40];
+    public IReadOnlyList<long> MyStatusPointer                  { get; } = [0x610A710, 0xA0, 0x40];
+    public IReadOnlyList<long> PartyPointer                     { get; } = [0x610A710, 0x18, 0x1B0, 0xF0, 0x50, 0x30, 0x0];
+    public IReadOnlyList<long> CurrentBoxPointer                { get; } = [0x610A710, 0xA8, 0x596];
 
-    public IReadOnlyList<long> LinkTradePartnerPokemonPointer   { get; } = [0x6108630, 0x128, 0x30, 0x0];
-    public IReadOnlyList<long> TradePartnerStatusPointer        { get; } = [0x6108630, 0x134];
-    public IReadOnlyList<long> TradePartnerBackupNIDPointer     { get; } = [0x6108630, 0x108];
+    public IReadOnlyList<long> LinkTradePartnerPokemonPointer   { get; } = [0x610D630, 0x128, 0x30, 0x0];
+    public IReadOnlyList<long> TradePartnerStatusPointer        { get; } = [0x610D630, 0x134];
+    public IReadOnlyList<long> TradePartnerBackupNIDPointer     { get; } = [0x610D630, 0x108];
 
-    public IReadOnlyList<long> LinkTradeCodeLengthPointer       { get; } = [0x6131138, 0x52];
-    public IReadOnlyList<long> LinkTradeCodePointer             { get; } = [0x6131138, 0x30, 0x0];
+    public IReadOnlyList<long> LinkTradeCodeLengthPointer       { get; } = [0x6136238, 0x52];
+    public IReadOnlyList<long> LinkTradeCodePointer             { get; } = [0x6136238, 0x30, 0x0];
 
-    public IReadOnlyList<long> LinkTradePartnerDataPointer      { get; } = [0x40F73D8, 0x1D8, 0x30, 0xA0, 0x0];
+    public IReadOnlyList<long> LinkTradePartnerDataPointer      { get; } = [0x40FC3D8, 0x1D8, 0x30, 0xA0, 0x0];
 
     public const uint TradePartnerNIDShift           = 0x30;
     public const uint TradePartnerTIDShift           = 0x74;
@@ -31,9 +31,9 @@ public class PokeDataOffsetsLZA
 
 
     // Main offsets
-    public const uint OverworldOffset = 0x6107858;
-    public const uint MenuOffset      = 0x61289C0;
-    public const uint ConnectedOffset = 0x612E358;
+    public const uint OverworldOffset = 0x610C858;
+    public const uint MenuOffset      = 0x612DA80;
+    public const uint ConnectedOffset = 0x6133458;
 
     public const int BoxFormatSlotSize = 0x148;
 }
