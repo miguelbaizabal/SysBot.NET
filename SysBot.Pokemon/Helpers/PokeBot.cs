f4167a13cdd473fa25fd7e93a788b15a03c92c86
Jon <90940611+jonklee99@users.noreply.github.com>
2026-05-22 08:27:23 -0500

    Update version to v1.8.5

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.8.4";
+        public const string Version = "v1.8.5";
     }
 }
