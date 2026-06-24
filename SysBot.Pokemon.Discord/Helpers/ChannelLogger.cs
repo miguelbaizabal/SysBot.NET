2ad25e38a57e9b80f8ea4ba9f7546d8822def9f8
Joseph11024 <joseph.olsen1989@gmail.com>
2026-06-23 19:28:33 -0500

    Missing permission logs
    Adds Server name, ID, channel and channel ID to identify precisely where exception is being thrown.

--- SysBot.Pokemon.Discord/Helpers/ChannelLogger.cs
+++ SysBot.Pokemon.Discord/Helpers/ChannelLogger.cs
@@ -1,7 +1,10 @@
+using Discord;
+using Discord.Net;
 using Discord.WebSocket;
 using SysBot.Base;
 
 using System;
+using System.Threading.Tasks;
 
 namespace SysBot.Pokemon.Discord;
 
@@ -16,12 +19,31 @@ public class ChannelLogger(ulong ChannelID, ISocketMessageChannel Channel) : ILo
         try
         {
             var text = GetMessage(message, identity);
-            Channel.SendMessageAsync(text);
+            _ = Channel.SendMessageAsync(text).ContinueWith(t =>
+            {
+                if (!t.IsFaulted || t.Exception == null)
+                    return;
+
+                var inner = t.Exception.InnerException ?? t.Exception;
+                string location = GetChannelLocation();
+
+                if (inner is HttpException httpEx && httpEx.HttpCode == System.Net.HttpStatusCode.Forbidden)
+                    LogUtil.LogError($"Missing Access sending log message. {location}", identity);
+                else
+                    LogUtil.LogError($"Failed to send log message. {location} Error: {inner.Message}", identity);
+            }, TaskContinuationOptions.OnlyOnFaulted);
         }
         catch (Exception ex)
         {
             LogUtil.LogSafe(ex, identity);
         }
+    }
+
+    private string GetChannelLocation()
+    {
+        if (Channel is SocketTextChannel tc)
+            return $"Server: {tc.Guild.Name} ({tc.Guild.Id}), Channel: #{tc.Name} ({ChannelID})";
+        return $"Channel: #{Channel.Name} ({ChannelID})";
     }
 
     private static string GetMessage(ReadOnlySpan<char> msg, string identity)
